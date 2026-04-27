7feb215cc44a6e0e42e34a286c6a68585fe1335f
Shubham Sharma <shubhamtech004@gmail.com>
2026-04-28 02:11:58 +0530

    change under client
    layout setup

---
+++ client/src/app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "../components/dasboard/Sidebar";
+import Topbar from "../components/dasboard/Topbar";
+
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        <main className="flex-1 p-6 bg-gray-100">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
