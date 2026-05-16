2777c3a00375706e5b6719f1575357358a79566a
Shubham Sharma <shubhamtech004@gmail.com>
2026-05-16 23:15:44 +0530

    debuging

--- server/src/routes/app.routes.js
+++ server/src/routes/app.routes.js
@@ -1,8 +1,6 @@
 import express from "express";
 
-import {
-  getMyDigitalId,
-} from "../controllers/app.controller.js";
+import { getMyDigitalId } from "../controllers/app.controller.js";
 
 const router = express.Router();
 
