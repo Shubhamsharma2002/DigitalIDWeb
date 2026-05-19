1f808f0b59511090a8f9c7c32f930637432447c6
Shubham Sharma <shubhamtech004@gmail.com>
2026-05-19 23:50:07 +0530

    debuging

--- server/src/index.js
+++ server/src/index.js
@@ -2,20 +2,15 @@ import "dotenv/config";
 import { server } from "./app.js";
 import connectDB from "./config/db.js";
 
-const PORT = process.env.PORT || 8000 ;
-
-
+const PORT = process.env.PORT || 8000;
 
 const startServer = async () => {
   try {
-
-    
     await connectDB();
 
     server.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
-
   } catch (error) {
     console.log("❌ Server startup failed", error.message);
   }
